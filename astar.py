e8c4637b796dedcd1abccc57b4bb207e3d87fcd4
Harris Davidson <harrisdavidson@harriss-mbp.olin.edu>
2017-10-30 17:23:07 -0400

    Add diagnal and hop movements

--- astar.py
+++ astar.py
@@ -74,12 +74,13 @@ class GridWorld():
     def _add_swamp(self, mouse_pos):
         """ Adds a swamp tile in the cell that mouse_pos indicates """
         swamp_coord = (mouse_pos[0]//50, mouse_pos[1]//50)
+        print('swamp!')
         if self._is_occupied(swamp_coord):
             if self.actors[swamp_coord].removable:
                 self.actors.pop(swamp_coord, None)
         elif swamp_coord != self.cake.cell_coordinates:
             swamp = ObstacleTile(swamp_coord, self, './images/swamp.jpg',
-                                is_unpassable=False, terrain_cost=8)
+                                is_unpassable=False, terrain_cost=3)
             self.actors[swamp_coord] = swamp
     def _add_lava(self, mouse_pos):
         """ Adds a lava tile in the cell that mouse_pos indicates """
@@ -203,14 +204,28 @@ class Paul(Actor):
         """ returns list of valid coords that are adjacent to the argument,
             open, and not in the closed list. """
         # modify directions and costs as needed
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        all_adj = [self.world._add_coords(coords, d) for d in directions]
-        in_bounds = [self.is_valid(c) for c in all_adj]
+        cardinals = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        diagonals = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        hops = [(2, 0), (0, 2), (-2, 0), (0, -2)]
+        all_card = [self.world._add_coords(coords, d) for d in cardinals]
+        all_diag = [self.world._add_coords(coords, d) for d in diagonals]
+        all_hops = [self.world._add_coords(coords, d) for d in hops]
+        in_bounds_card = [self.is_valid(c) for c in all_card]
+        in_bounds_diag = [self.is_valid(c) for c in all_diag]
+        in_bounds_hops = [self.is_valid(c) for c in all_hops]
         costs = []
         open_adj = []
-        for i, coord in enumerate(all_adj):
-            if(in_bounds[i]):
+        for i, coord in enumerate(all_card):
+            if(in_bounds_card[i]):
                 costs.append(1 + self.world.get_terrain_cost(coord))
+                open_adj.append(coord)
+        for i, coord in enumerate(all_diag):
+            if(in_bounds_diag[i]):
+                costs.append(3 + self.world.get_terrain_cost(coord))
+                open_adj.append(coord)
+        for i, coord in enumerate(all_hops):
+            if(in_bounds_hops[i]):
+                costs.append(8 + self.world.get_terrain_cost(coord))
                 open_adj.append(coord)
         return open_adj, costs
 
